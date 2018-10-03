0d050bbebaffa1b2414f42b5b55b44675f292622
Witold Mrózek <wmrozek89@gmail.com>
2018-10-03 23:00:19 +0200

    Dodawanie nowego filmu

--- cinema_first/src/main/java/com/sda/cinema/administration/AdminCommand.java
+++ cinema_first/src/main/java/com/sda/cinema/administration/AdminCommand.java
@@ -3,10 +3,7 @@ package com.sda.cinema.administration;
 import com.sda.cinema.model.Genre;
 import com.sda.cinema.model.Movie;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.sql.*;
 
 public class AdminCommand {
     private static final String INSERT_MOVIE = "INSERT INTO movies(title, " +
@@ -28,22 +25,33 @@ public class AdminCommand {
                                    String genreName) {
         try {
             Genre genre = AdminQuery.getGenreByName(connection, genreName);
-            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_MOVIE);
-            preparedStatement.setString(1, title);
-            preparedStatement.setString(2, releaseDate);
-            preparedStatement.setString(3, actors);
-            preparedStatement.setString(4, director);
-            preparedStatement.setString(5, duration);
-            preparedStatement.setString(6, description);
-            if (genre != null) {
-                preparedStatement.setInt(7, genre.getId());
-            } else {
-                preparedStatement.setNull(7, Types.INTEGER);
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement(INSERT_MOVIE, Statement.RETURN_GENERATED_KEYS);
+            fillParameters(title, director, actors, description, releaseDate, duration, genre, preparedStatement);
+
+            int rowsAdded = preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            while(resultSet.next()){
+                System.out.println(resultSet.getInt(1));
             }
-            return preparedStatement.executeUpdate() > 0;
+            return rowsAdded > 0;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
+        }
+    }
+
+    private static void fillParameters(String title, String director, String actors, String description, String releaseDate, String duration, Genre genre, PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement.setString(1, title);
+        preparedStatement.setString(2, releaseDate);
+        preparedStatement.setString(3, actors);
+        preparedStatement.setString(4, director);
+        preparedStatement.setString(5, duration);
+        preparedStatement.setString(6, description);
+        if (genre != null) {
+            preparedStatement.setInt(7, genre.getId());
+        } else {
+            preparedStatement.setNull(7, Types.INTEGER);
         }
     }
 
